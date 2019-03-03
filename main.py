46bca7517c8685591e6a0b2215e11370a5a8441b
jstefan <jstefan@umich.edu>
2019-03-03 15:45:36 -0500

    more retrieval options

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from update import insertData
-from analyze import getRecentData
+from analyze import *
 
 def main():
 
     #insertData('DOW30')
     #insertData('NASDAQ100')
-    getRecentData('DOW30')
+    #print(getRecentDOW())
+    #print(getRecentNASDAQ())
+    #print(getDOWStock('AAPL'))
+    #print(getNASDAQStock('CSCO'))
 
 if __name__ == "__main__":
     main()
