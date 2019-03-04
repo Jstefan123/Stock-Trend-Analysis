1f676aa558c2e2b27c590146e1f9a8fd7c6c479c
jstefan <jstefan@umich.edu>
2019-03-03 22:24:16 -0500

    added ticker arrays to config

--- config.py
+++ config.py
@@ -19,3 +19,20 @@ db_file = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
     'StockTrender', 'data', 'database.sqlite3')
 db = sqlite3.connect(db_file, isolation_level=None).cursor()
+
+# list of DOW30 tickers for plotting X axis
+dow30_tickers = ['AXP', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'DWDP', 'XOM', 'GS',
+'HD', 'IBM', 'INTC', 'JNJ', 'KO', 'JPM', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE',
+'PFE', 'PG', 'TRV', 'UNH', 'UTX', 'VZ', 'V', 'WBA', 'WMT', 'DIS']
+
+nasdaq100_tickers = ['ATVI', 'ADBE', 'AMD', 'ALGN', 'ALXN', 'AMZN', 'AMGN',
+'AAL', 'ADI', 'AAPL', 'AMAT', 'ASML', 'ADSK', 'ADP', 'AVGO', 'BIDU', 'BIIB',
+'BMRN', 'CDNS', 'CELG', 'CERN', 'CHKP', 'CHTR', 'CTRP', 'CTAS', 'CSCO', 'CTXS',
+'CMCSA', 'COST', 'CSX', 'CTSH', 'DLTR', 'EA', 'EBAY', 'EXPE', 'FAST', 'FB',
+'FISV', 'FOX', 'FOXA', 'GILD', 'GOOG', 'GOOGL', 'HAS', 'HSIC', 'ILMN', 'INCY',
+'INTC', 'INTU', 'ISRG', 'IDXX', 'JBHT', 'JD', 'KLAC', 'KHC', 'LRCX', 'LBTYA',
+'LBTYK', 'LULU', 'MELI', 'MAR', 'MCHP', 'MDLZ', 'MNST', 'MSFT', 'MU', 'MXIM',
+'MYL', 'NTAP', 'NFLX', 'NTES', 'NVDA', 'NXPI', 'ORLY', 'PAYX', 'PCAR', 'BKNG',
+'PYPL', 'PEP', 'QCOM', 'REGN', 'ROST', 'SIRI', 'SWKS', 'SBUX', 'SYMC', 'SNPS',
+'TTWO', 'TSLA', 'TXN', 'TMUS', 'ULTA', 'UAL', 'VRSN', 'VRSK', 'VRTX', 'WBA',
+'WDC', 'WDAY', 'WLTW', 'WYNN', 'XEL', 'XLNX']
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from update import insertData
-from database import *
 
 def main():
-    print("hi")
 
 if __name__ == "__main__":
     main()
