9267579eb06415699ccb082abf002dcc6d652b35
jstefan <jstefan@umich.edu>
2019-03-05 16:32:48 -0500

    test yml update

--- news.py
+++ news.py
@@ -1,5 +1,5 @@
 import config
-from newsapi.newsapi_client import NewsApiClient
+from newsapi-python import NewsApiClient
 from datetime import datetime, timedelta
 from textblob import TextBlob
 import json
