e0166a2cca7a3008e0da19d58b29b075b981f536
jstefan <jstefan@umich.edu>
2019-03-05 16:56:23 -0500

    test yml update

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ num_hours = 24
 news_api_key = "999e5995615f44ba8ed4cece981b1a09"
 
 # for twitter api
-twitter_api_key = "MmB5TCkf7PVCzxbgCCXh0C9I6"
-twitter_api_secret_key = "BqRHDkKJzJTTEzadfLtI3vQalyNc9gPjdMxOdM7pVLaMgq2pl4"
-twitter_token = "1101628164588167173-TZX6niJlRttzpQuMmXLDpcnDBJoFRY"
-twitter_secret_token = "6JiH6DP1nB2LmISJoiT4iC5yWwtoGpzf5sBgQzAtyR2fW"
+twitter_api_key = "NJWHH6Zyv9ONco2KuYrmWHArm "
+twitter_api_secret_key = "43GiTYAh61eTgatGk44hTi6fBV7Zw6v7E8AK65LwEYHmggO92w"
+twitter_token = "1103049068052402177-nEkhHawLE76okjfEzlkDoLtiw9gdkF"
+twitter_secret_token = "SizLLo62icfJU7eK7XDZrdjfUvPhio3jPPCT6Xhful1dB"
 
 # create cursor to connect to database
 db_file = os.path.join(
